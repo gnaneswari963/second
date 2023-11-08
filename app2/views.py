24b28d5d686bc181d6f69239fb6855b4c08f5b9b
gnaneswari963 <gnaneswari22092000@gmail.com>
2023-11-08 12:22:19 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def pyspider(request):
+    return HttpResponse("hlo python full stack developer")
